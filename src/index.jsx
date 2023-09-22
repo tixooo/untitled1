dee75921c7537b998335951f37aeb52b45d851d5
Stefanov <konstantin.stefanov@dxc.com>
2023-09-22 17:23:19 +0300

    adding first component

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './GreetingsComponent.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(<Greeting/>);
